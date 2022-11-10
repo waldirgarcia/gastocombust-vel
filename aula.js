e9442b0f60fcf0bfc092ee0b5964e293005fbe56
Waldir Neto <50805130+waldirgarcia@users.noreply.github.com>
2022-11-10 20:36:14 -0300

    Primeiro desafio JS
    Essa foi tranquilo

---
+++ aula.js
@@ -0,0 +1,13 @@
+
+
+/* Saber valor a gastar em uma viagem de 100km*/
+
+/* dividir a distância por a autonomia do carro em 
+gasto por km e multiplica-la por o valor / variáel combustível*/
+
+
+const gasolina = 5.79;
+const autonomia = 10; 
+const distância = 100;
+
+console.log (distância/autonomia*gasolina);
